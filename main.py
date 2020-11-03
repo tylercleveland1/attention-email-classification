6c0cd935c20fbc1b92f2506bb25abeca919309d9
Tyler Cleveland <tyler@focal.ws>
2020-11-03 14:40:33 -0800

    Add transformer model with self-attention

--- main.py
+++ main.py
@@ -6,32 +6,60 @@
 #https://androidkt.com/text-classification-using-attention-mechanism-in-keras/
 #https://www.tensorflow.org/api_docs/python/tf/keras/layers/Attention
 
-from spam_dataset import SpamDataset
 import tensorflow as tf
+import keras
 from keras_preprocessing import sequence
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from spam_dataset import SpamDataset
+from layers import *
 
 def main():
     n_words = 10000
     embedding_dim = 50
-    max_len = 250
+    max_len = 50 #only take top 50 words
 
 
     X, y = SpamDataset.get_data()
 
     #tokenize
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=n_words, split=' ')
+    tokenizer = keras.preprocessing.text.Tokenizer(num_words=n_words, split=' ')
     tokenizer.fit_on_texts(X)
     X = tokenizer.texts_to_sequences(X)
 
     #pad sequences
     X = sequence.pad_sequences(X, maxlen=max_len, truncating='post', padding='post', value=0)
+    
+    X = np.array([np.array(x).astype('float32') for x in X])
+    y = np.array(y).astype('float32')
 
-    #create model
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Embedding(n_words, embedding_dim, input_length = X.shape[1]))
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    embed_dim = 32
+    num_heads = 1
 
+    transformer_layer = TransformerBlock(embed_dim, num_heads, 50)
+    embedding_layer = TokenAndPositionEmbedding(max_len, n_words, embed_dim)
+
+    inputs = layers.Input(shape=(max_len,))
+    x = embedding_layer(inputs)
+    x = transformer_layer(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(20, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    outputs = layers.Dense(2, activation="softmax")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    model.summary()
+    
+    model.fit(X_train, y_train, epochs=4)
+
+    model.evaluate(X_test, y_test)
 
 if __name__ == "__main__":
     main()
