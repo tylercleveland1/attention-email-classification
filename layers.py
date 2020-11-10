f1112661abbeb466ba6694bc58dbe853d8b24b67
Tyler Cleveland <tyler@focal.ws>
2020-11-09 17:20:01 -0800

    Remove positional embedding, replace custom TokenAndPositionEmbedding layer with single token keras.Embedding layer

--- layers.py
+++ layers.py
@@ -30,16 +30,3 @@ class SelfAttention(layers.Layer):
         value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
         attention, weights = self.attention(query, key, value)
         return attention
-
-class TokenAndPositionEmbedding(layers.Layer):
-    def __init__(self, maxlen, vocab_size, embed_dim):
-        super(TokenAndPositionEmbedding, self).__init__()
-        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
-        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
-
-    def call(self, x):
-        maxlen = tf.shape(x)[-1]
-        positions = tf.range(start=0, limit=maxlen, delta=1)
-        positions = self.pos_emb(positions)
-        x = self.token_emb(x)
-        return x + positions
